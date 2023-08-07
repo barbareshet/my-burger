a4a476bc3183b930f40064ddf833c7e30d6aeb1f
ido barnea <barbareshet@gmail.com>
2023-08-07 20:14:34 +0300

    better performance

--- src/components/Burger/Order/Order.js
+++ src/components/Burger/Order/Order.js
@@ -1,35 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AuxWrap from "../../hoc/AuxWrap";
 import Button from "../../UI/Button/Button";
 import fixedPrice from "../../../utils/fixedPrice";
-const Order = (props) => {
+class Order extends Component{
 
+    
 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return <li key={igKey}><span style={{textTransform:"capitalize"}}>{igKey} : {props.ingredients[igKey]}</span></li>
-        })
-    return(
-        <AuxWrap className="Order">
-            <h3 className="modal-title">{props.title}</h3>
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return <li key={igKey}><span style={{textTransform:"capitalize"}}>{igKey} : {this.props.ingredients[igKey]}</span></li>
+            })
+        return(
+            <AuxWrap className="Order">
+                <h3 className="modal-title">{this.props.title}</h3>
 
-            <div className="modal-content">
-                {props.description}
-            </div>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <h4 className="total">
-                <span style={{color:"#ce5054"}}>
-                    Total Price: ${fixedPrice(props.totalPrice)}
-                </span>
-            </h4>
-            <div className="modal-footer">
-                <Button type="place-order" handleButtonClick={props.handleContiniueOrder}>Place Order</Button>
-                <Button type="cancel" handleButtonClick={props.handleCancelOrder}>Cancel</Button>
-            </div>
-        </AuxWrap>
-    )
+                <div className="modal-content">
+                    {this.props.description}
+                </div>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <h4 className="total">
+                    <span style={{color:"#ce5054"}}>
+                    Total Price: ${fixedPrice(this.props.totalPrice)}
+                    </span>
+                </h4>
+                <div className="modal-footer">
+                    <Button type="place-order" handleButtonClick={this.props.handleContiniueOrder}>Place Order</Button>
+                    <Button type="cancel" handleButtonClick={this.props.handleCancelOrder}>Cancel</Button>
+                </div>
+            </AuxWrap>
+        )
+    }
+
 };
 
 export default Order;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AuxWrap from "../../hoc/AuxWrap";
 import classes from './Modal.css'
 import Backdrop from "../Backdrop/Backdrop";
-const Modal = (props) => {
-    const {modalClosed, show} = props
-    return (
-        <AuxWrap>
-            <Backdrop show={show} handleBackdrop={modalClosed}/>
-            <div
-                className="Modal"
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                {props.children}
-            </div>
-        </AuxWrap>
-    );
+import {update} from "bfj/src/memory";
+class Modal extends Component{
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('update')
+    }
+
+    render() {
+        const {modalClosed, show} = this.props
+        return (
+            <AuxWrap>
+                <Backdrop show={show} handleBackdrop={modalClosed}/>
+                <div
+                    className="Modal"
+                    style={{
+                        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </AuxWrap>
+        );
+    }
 }
 
 export default Modal;
